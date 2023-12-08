79e4b8c5095cbe33c2327257c57aad9c25406736
Ádrian Gama <adrian.gama.se@gmail.com>
2023-12-08 15:33:15 -0300

    Add generic `Problem` class and possible A* solution

---
+++ a-star.cpp
@@ -0,0 +1,43 @@
+#include "problem.hpp"
+#include <algorithm>
+#include <memory>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+vector<Problem::State> astar(Problem problem) {
+  priority_queue<shared_ptr<Node>, vector<shared_ptr<Node>>, Node::Comparator>
+      frontier;
+  frontier.push(make_shared<Node>(problem.initial, nullptr, 0));
+
+  unordered_map<Problem::State, shared_ptr<Node>, Problem::State::Hash>
+      explored;
+
+  while (!frontier.empty()) {
+    shared_ptr<Node> current = frontier.top();
+    frontier.pop();
+
+    if (problem.goal_test(current->state)) {
+      vector<Problem::State> path;
+      auto node = current;
+      while (node != nullptr) {
+        path.push_back(node->state);
+        node = node->parent;
+      }
+      reverse(path.begin(), path.end());
+      return path;
+    }
+
+    explored[current->state] = current;
+    for (auto action : problem.actions(current->state)) {
+      auto next = problem.result(current->state, action);
+      auto cost = problem.path_cost(current->state, action, next);
+      if (explored.find(next) == explored.end() || cost < explored[next]->cost)
+        frontier.push(
+            make_shared<Node>(next, current, cost + problem.heuristics(next)));
+    }
+  }
+
+  return {};
+}
---
+++ problem.hpp
@@ -0,0 +1,44 @@
+#ifndef PROBLEM_HPP
+#define PROBLEM_HPP
+
+#include <cstddef>
+#include <memory>
+#include <vector>
+using namespace std;
+
+class Problem {
+public:
+  struct State {
+    struct Hash {
+      size_t operator()(const State &state) const;
+    };
+  };
+  struct Action {};
+
+  State initial;
+  State goal;
+
+  State result(const State &state, const Action &action);
+  int path_cost(const State &current, const Action &action, const State &next);
+  int heuristics(const State &next);
+  bool goal_test(const State &current);
+  vector<Action> actions(const State &current);
+};
+
+struct Node {
+  Problem::State state;
+  shared_ptr<Node> parent;
+  int cost;
+
+  Node(Problem::State state, shared_ptr<Node> parent, int cost)
+      : state(state), parent(parent), cost(cost) {}
+
+  struct Comparator {
+    bool operator()(const shared_ptr<Node> lhs,
+                    const shared_ptr<Node> rhs) const {
+      return lhs->cost > rhs->cost;
+    }
+  };
+};
+
+#endif
