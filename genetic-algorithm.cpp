1eaefec52c9fb6430ba82dd9f7089db4c5af98a6
Ádrian Gama <adrian.gama.se@gmail.com>
2023-12-15 22:12:50 -0300

    Add Genetic Algorithm with problem solution - Fix `nqueen_threshold` does not return correct value - Fix `nqueen_fitness` return type - Fix `nqueen_fitness` return type - Add logic to `mutate` that guarantees different gene - Fix `selection` logic by using uniform distribution - Generate weights outside `genetic_algorithm` using `weights_by` - Check `fitthreshold` inside generation building - Add generation limits - Print correct solution

--- genetic-algorithm.cpp
+++ genetic-algorithm.cpp
@@ -1,20 +1,25 @@
 #include <cstddef>
 #include <cstdlib>
+#include <ctime>
 #include <functional>
 #include <iostream>
+#include <memory>
 #include <ostream>
+#include <random>
 #include <utility>
 #include <vector>
 using namespace std;
 
 using NQueenState = int;
+const ssize_t NOGENLIMIT = -1;
 
 int nqueen_fitthreshold(size_t table_size) {
-  return table_size * (table_size - 1 / 2);
+  return table_size * ((table_size - 1) / 2.0);
 }
 
-NQueenState nqueen_fitness(vector<NQueenState> individual) {
-  NQueenState collisions = 0;
+template <typename S>
+int nqueen_fitness(vector<S> individual) {
+  int collisions = 0;
   for (int i = 0; i < individual.size() - 1; i++) {
     for (int j = 1; j < individual.size(); j++) {
       int target = individual[i + j];
@@ -26,9 +31,8 @@ NQueenState nqueen_fitness(vector<NQueenState> individual) {
 
       if ((forward_move == target) ||
           (at_bounds_upwards && forward_ascending_move == target) ||
-          (at_bounds_downwards && forward_descending_move == target)) {
+          (at_bounds_downwards && forward_descending_move == target))
         collisions++;
-      }
     }
   }
   return nqueen_fitthreshold(individual.size()) - collisions;
@@ -38,6 +42,9 @@ template <typename S>
 vector<S> mutate(vector<S> individual, vector<S> genes) {
   int n = rand() % individual.size();
   int m = rand() % genes.size();
+  if (individual[n] == m)
+    m = (m + 1) % genes.size();
+
   individual[n] = genes[m];
   return individual;
 }
@@ -55,23 +62,27 @@ vector<S> reproduce(vector<S> parentx, vector<S> parenty) {
   return child;
 }
 
+// https://stackoverflow.com/questions/56723551
 template <typename S>
 pair<vector<S>, vector<S>> selection(vector<vector<S>> population, vector<int> weights) {
   int total = 0;
   for (int weight : weights)
     total += weight;
-  int rnd = rand() % total;
 
-  vector<int> parents;
+  random_device rd;
+  mt19937 gen(rd());
+  uniform_int_distribution<int> distribution(0, total);
+  auto rnd = distribution(gen);
+
+  vector<vector<S>> parents;
   int accumulated = 0;
   for (int i = 0; i < population.size() && parents.size() < 2; i++) {
     accumulated += weights[i];
-    if (rnd <= accumulated) {
+    if (rnd <= accumulated)
       parents.push_back(population[i]);
-    }
   }
 
-  for (int i = population.size() - 1; parents.size() < 2; i++)
+  for (int i = population.size() - 1; parents.size() < 2; i--)
     if (parents.size() == 0 || parents[0] != population[i])
       parents.push_back(population[i]);
 
@@ -79,33 +90,64 @@ pair<vector<S>, vector<S>> selection(vector<vector<S>> population, vector<int> w
 }
 
 template <typename S>
-vector<S> genetic_algorithm(vector<vector<S>> &population, function<vector<int>(vector<S>)> fitness, vector<S> genes, int fitthreshold = 0, int genlimit = 1000, double mutation_probability = 0.05) {
-  while (genlimit > 0) {
-    vector<int> weights;
-    for (auto individual : population)
-      weights.push_back(fitness(individual));
+vector<int> weight_by(vector<vector<S>> population, function<int(vector<S>)> fitness) {
+  vector<int> weights(population.size());
+  for (int i = 0; i < population.size(); i++)
+    weights[i] = fitness(population[i]);
+  return weights;
+}
 
-    if (fitthreshold != 0) {
-      vector<S> fittest;
-      for (int i = 0; i < population.size(); i++)
-        if (weights[i] >= fitthreshold)
-          return population[i];
-    }
+template <typename S>
+vector<S> genetic_algorithm(vector<vector<S>> population, function<int(vector<S>)> fitness, vector<S> genes, int fitthreshold = 0, ssize_t genlimit = 10000, double mutation_probability = 0.1, shared_ptr<size_t> gens = NULL) {
+  while (genlimit > 0 || genlimit == NOGENLIMIT) {
+    auto weights = weight_by(population, fitness);
 
-    vector<S> generation;
+    vector<vector<S>> generation(population.size());
     for (int i = 0; i < population.size(); i++) {
       auto [parentx, parenty] = selection(population, weights);
       auto child = reproduce(parentx, parenty);
       if (rand() / static_cast<double>(RAND_MAX) < mutation_probability)
         child = mutate(child, genes);
 
-      generation.push_back(child);
+      if (fitthreshold != 0)
+        for (int i = 0; i < population.size(); i++)
+          if (weights[i] >= fitthreshold)
+            return population[i];
+
+      generation[i] = child;
     }
     population = generation;
-    genlimit--;
+
+    if (gens != NULL)
+      (*gens)++;
+
+    if (genlimit != -1)
+      genlimit--;
   }
 
   return population[0];
 }
 
-int main(void) { cout << "Hello, World" << endl; }
+int main(void) {
+  srand(time(NULL));
+  size_t table_size = 8;
+  vector<NQueenState> genes = {1, 2, 3, 4, 5, 6, 7, 8};
+  size_t population_size = 100;
+  vector<vector<NQueenState>> population;
+  shared_ptr<size_t> gens = make_shared<size_t>(0);
+
+  for (int i = 0; i < population_size; i++) {
+    population.push_back({});
+    for (int j = 0; j < table_size; j++)
+      population[i].push_back(rand() % table_size + 1);
+  }
+
+  auto fittest = genetic_algorithm<NQueenState>(population, nqueen_fitness<NQueenState>, genes, nqueen_fitthreshold(table_size), NOGENLIMIT, 0.1, gens);
+
+  cout << "Fittest individual with " << *gens << " generations: ";
+  cout << "[ ";
+  for (auto gene : fittest) {
+    cout << gene << " ";
+  }
+  cout << "]" << endl;
+}
